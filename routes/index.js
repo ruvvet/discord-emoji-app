736e99b5b1d476f1d04690b08b212b6eb648fbfe
Jenny <finej01@gmail.com>
2020-11-30 23:11:18 -0500

    updated error pages

--- routes/index.js
+++ routes/index.js
@@ -9,12 +9,12 @@ router.use('/', require('./public'));
 router.use('/', require('./protected'));
 
 router.use(function (req, res, next) {
-  res.status(404).send("Sorry can't find that!");
+  res.status(404).render('./error/404');
 });
 
 router.use(function (err, req, res, next) {
   console.error(err.stack);
-  res.status(500).send('Something broke!');
+  res.status(500).render('./error/500');
 });
 
 module.exports = router;
